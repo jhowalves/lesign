97f9cc61a6a29ff054988b4b746c1ab49a93e3c0
Jonatas Alves <jonatas.alves@fcamara.com.br>
2021-07-06 21:19:42 -0300

    add react in components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './styles.scss';
 import facebook from '../../assets/facebook.png'
 
--- src/pages/Company/index.js
+++ src/pages/Company/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './styles.scss';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
